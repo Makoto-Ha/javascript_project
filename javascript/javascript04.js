2e2b6f25acdc141ac3c293b75534b0b8ce411066
eric <s51r3040@gmail.com>
2021-04-11 23:43:34 +0800

    javascript if判斷練習

---
+++ javascript/javascript04.js
@@ -0,0 +1,10 @@
+var answer = 100
+if(answer == 100){
+    alert('獎勵一輛BMW');
+}else if(answer >= 80 && answer <= 99){
+    alert('獎勵一台inphone15s');
+}else if(answer >= 60 && answer < 80){
+    alert('獎勵一本參考書');
+}else{
+    alert('可惜，你這成績什麼都沒有');
+}
