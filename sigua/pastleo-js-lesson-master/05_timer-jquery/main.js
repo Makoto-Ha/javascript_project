32f21c4b189fdc27cc54e642fd2442af5cfa1a5b
eric <s51r3040@gmail.com>
2021-04-17 19:53:08 +0800

    javascript練習

--- sigua/pastleo-js-lesson-master/05_timer-jquery/main.js
+++ sigua/pastleo-js-lesson-master/05_timer-jquery/main.js
@@ -25,29 +25,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-$(document).ready(function() {
-  let timer, cnt = 0;
-  $('button').click(function() {
-    if (timer){
-      clearInterval(timer)
-      timer = null;
+// $(document).ready(function() {
+//   let timer, cnt = 0;
+//   $('button').click(function() {
+//     if (timer){
+//       clearInterval(timer)
+//       timer = null;
 
-      $('button').text('start')
-      $('#running').slideUp();
-    }else {
-      timer = setInterval(function() {
-        cnt += 1
-        $('#seconds').text(Math.floor(cnt / 10))
+//       $('button').text('start')
+//       $('#running').slideUp();
+//     }else {
+//       timer = setInterval(function() {
+//         cnt += 1
+//         $('#seconds').text(Math.floor(cnt / 10))
 
-        $('.dots div:last-child').prependTo($('.dots'))
+//         $('.dots div:last-child').prependTo($('.dots'))
 
-        }, 100)
+//         }, 100)
 
 
-    $('button').text('Stop')
-    $('#running').slideDown();
-    }
-  })
-})
+//     $('button').text('Stop')
+//     $('#running').slideDown();
+//     }
+//   })
+// })
 
 
